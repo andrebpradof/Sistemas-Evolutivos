503fd880fa3fb0de8b17e1cc0652d0d533c5e1c4
andrebpradof <andrebpf@usp.br>
2022-01-16 21:35:11 -0300

    Co-authored-by: Mateus Fernandes Doimo <mateusdoimo@usp.br> Co-authored-by: Renan Peres Martins <renanperes99@usp.br>

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ F_PROBABILITY = 0.5
 MUTATION_RATE = 1
 
 def createBoard(n):
-    board = np.zeros((n, n))
+    board = np.zeros([n, n], dtype=int)
     return board
 
 
 def cleanBoard(board, n):
-    board = np.zeros((n, n))
+    board = np.zeros([n, n], dtype=int)
     return board
 
 
 def initializesPopulation(n):
-    population = np.zeros(MAX_POPULATION,n)
+    population = np.zeros([MAX_POPULATION,n], dtype=int)
     for i in range(MAX_POPULATION):
         for j in range(n):
             population[i][j] = randrange(start=0,stop=n)
     return population
 
 def generateFitnessVector(population, board, n):
-    fitness_population = np.zeros(MAX_POPULATION)
+    fitness_population = np.zeros(MAX_POPULATION, dtype=int)
     for i in range(MAX_POPULATION):
         fitness_population[i], board = valuationSample(board, population[i],n)
     return fitness_population, board
@@ -55,13 +55,13 @@ def recombinesTournamentIndividuals(population, n, fitness):
 
         # DELETAR ISSO apaga_populacao(*population);
         
-    return newPopulation
+    return newPopulation, indexBetterFitness
         
 def selectFather(fitness):
     indA = randrange(start=0,stop=MAX_POPULATION)
     indB = randrange(start=0,stop=MAX_POPULATION)
     
-    num =  (randrange(start=0,stop=1000))/1000
+    num =  (randrange(start=0,stop=1000))/1000.0
 
     if(num < F_PROBABILITY):
         if (fitness[indA] >  fitness[indB]):
@@ -84,8 +84,7 @@ def recombinesIndividualsElitism(population, n, fitness):
             betterFitness = fitness[i]
 
     for i in range(MAX_POPULATION):
-        limit =  randrange(start=0,stop=n)
-        for j in range(MAX_POPULATION):
+        for j in range(n):
             population[i][j] = population[indexBetterFitness][j]
             
     # VERIFICAR RETTORNO AQUIIII
@@ -104,6 +103,7 @@ def mutation(population, n, mutationRate):
                     population[i][genome] += 1
                 else:
                     population[i][genome] -=1
+    return population
 
 def randomFeeding(n):
     vector = [randrange(start=0,stop=n) for i in range(n)]
@@ -112,7 +112,7 @@ def randomFeeding(n):
 
 def valuationSample(board, sample, n):
     sumFitness = 0
-    board = cleanBoard(n)
+    board = cleanBoard(board, n)
     for i in range(n):
         board = insertPiece(board, i, sample[i], 1)
     for i in range(n):
@@ -125,10 +125,6 @@ def valuationSample(board, sample, n):
 def insertSample(sample, board, n):
     for i in range(n):
         board[i][sample[i]] = 1
-    return 1, board
-
-def cleanBoard(n):
-    board = [[0 for col in range(n)] for row in range(n)]
     return board
 
 def insertPiece(board, position_X, position_Y, typePiece):
@@ -154,11 +150,21 @@ def verifyAttack(board, n, position_X, position_Y):
         
     if typeCheck == 1 or typeCheck == 2:
         for i in range(n):
-            if board[position_X][i] != 0 and i != position_X:
-                atk += 1               
+            if i == position_Y:
+                continue
+            if board[position_X][i] != 0:
+                atk += 1
         for i in range(n):
-            if board[i][position_Y] != 0 and i != position_Y:
-                atk += 1  
+            if i == position_X:
+                continue
+            if board[i][position_Y] != 0:
+                atk += 1
+        # for i in range(n):
+        #     if board[position_X][i] != 0 and i != position_Y:
+        #         atk += 1               
+        # for i in range(n):
+        #     if board[i][position_Y] != 0 and i != position_X:
+        #         atk += 1  
                              
     if typeCheck == 1 or typeCheck == 3:
         i = 1
@@ -168,21 +174,21 @@ def verifyAttack(board, n, position_X, position_Y):
             i += 1
         i = 1
         while position_X - i >= 0 and position_Y - i >= 0:
-            if board[position_X + i][position_Y + i] != 0:
+            if board[position_X - i][position_Y - i] != 0:
                 atk += 1
             i += 1
         i = 1
         while position_X + i < n and position_Y - i >= 0:
-            if board[position_X + i][position_Y + i] != 0:
+            if board[position_X + i][position_Y - i] != 0:
                 atk += 1
             i += 1
         i = 1
         while position_X - i >= 0 and position_Y + i < n:
-            if board[position_X + i][position_Y + i] != 0:
+            if board[position_X - i][position_Y + i] != 0:
                 atk += 1
             i += 1
             
-    return atk
+    return atk, board
 
 def printBoard(board, n):
     for i in range(n):
@@ -197,7 +203,7 @@ def main():
     board = createBoard(n)
     genocides = 0
     stop_criterion = 1
-    best_individual = np.zeros(n)
+    best_individual = np.zeros(n, dtype=int)
     generation = 0
     mutation_rate = 1
     fitness_previous = 1000000
@@ -208,24 +214,25 @@ def main():
             for i in range(n):
                 population[0][i] = best_individual[i]
         while True:
-            fitness = generateFitnessVector(population,board,n)
-            index_best_individual = recombinesIndividualsElitism(population,n,fitness)
+            fitness, board = generateFitnessVector(population,board,n)
+            population, index_best_individual = recombinesTournamentIndividuals(population, n, fitness)
+            #index_best_individual, population = recombinesIndividualsElitism(population,n,fitness)
             for i in range(n):
                 best_individual[i] = population[index_best_individual][i]
-            mutation(population,n,mutation_rate)
-            cleanBoard(board,n)
-            insertSample(population[best_individual],board, n)
+            population = mutation(population,n,mutation_rate)
+            board = cleanBoard(board,n)
+            board = insertSample(population[best_individual],board, n)
             #printBoard(board,n)
-            generation = generation+1
-            fitness_best_individual = valuationSample(board, best_individual, n)
+            generation += 1
+            fitness_best_individual, board = valuationSample(board, best_individual, n)
             #print("Generation: ",generation)
             #print("fitness: ",fitness_best_individual)
             if fitness_best_individual == 0:
-                insertSample(best_individual, board, n)
+                board = insertSample(best_individual, board, n)
                 printBoard(board, n)
-                #print("Generation: ",generation)
-                #print("genocidios: ", genocides)
-                #print("fitness: ",fitness_best_individual)
+                print("Generation: ",generation)
+                print("genocidios: ", genocides)
+                print("fitness: ",fitness_best_individual)
                 stop_criterion = 0
                 break
             if fitness_best_individual == fitness_previous:
